98e305de01cfdf434c2206f86a22adf24fc85f6d
Samuel Kaiser <samuel.kaiser01@gmail.com>
2017-04-16 22:49:05 +0200

    state saved locally

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react'
 import {render} from 'react-dom'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
+import {loadState, saveState} from './local-storage'
 import App from './App'
-import reducers from './reducers'
+import soundboardApp from './reducers'
 import './index.css'
 
+// Video tutorial on persisting Redux states
+// https://egghead.io/lessons/javascript-redux-persisting-the-state-to-the-local-storage
+const persistedState = loadState()
 const store = createStore(
-  reducers,
+  soundboardApp,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
 
 render(
   <Provider store={store}><App/></Provider>,
