acd7b62aae69d51f6f38db5475904b3d30aaaf9d
Samuel Kaiser <samuel.kaiser01@gmail.com>
2017-04-16 16:44:29 +0200

    added delete actions

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,9 @@
 import {createAction} from 'redux-act'
 
 export const addSoundboard = createAction('add a soundboard')
+export const deleteSoundboard = createAction('delete a soundboard')
 export const addSound = createAction('add a sound')
+export const deleteSound = createAction('delete a sound')
 export const addSoundToSoundboard = createAction('add a sound to a soundboard')
 export const setCurrentSoundboard = createAction('set the current soundboard')
 export const setCurrentView = createAction('set the current view')
