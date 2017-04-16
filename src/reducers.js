c2d4874cbf917d61c31b54191ba04425d431d7cb
Samuel Kaiser <samuel.kaiser01@gmail.com>
2017-04-16 16:20:14 +0200

    preparing for rewrite

--- src/reducers.js
+++ src/reducers.js
@@ -1,57 +1,12 @@
-// const initialState = {
-//   soundboard: 0,
-//   view: 'soundboards',
-//   soundboards: [
-//     {
-//       index: 0,
-//       name: 'Soundboard',
-//       sounds: [{index: 0, name: 'Beep', src: '/beep.mp3'}],
-//       locked: false
-//     }
-//   ]
-// }
+const soundboardTemplate = {
+  name: 'Unnamed',
+  sounds: [],
+  locked: false
+}
 
 const initialState = {
   currentSoundboard: 0,
   currentView: 'soundboards',
-  soundboards: [
-    {
-      name: 'Soundboard',
-      sounds: [0],
-      locked: false
-    }
-  ],
-  sounds: [{name: 'Beep', src: '/beep.mp3'}]
-}
-
-export default (state = initialState, action) => {
-  switch (action.type) {
-    case 'ADD_SOUNDBOARD': return {
-      ...state,
-      soundboards: [...state.currentSoundboards, {
-        index: state.currentSoundboards.length, name: action.name
-      }]
-    }
-    case 'ADD_SOUND': return {
-      ...state,
-      view: 'soundboard',
-      soundboards: [
-        ...state.currentSoundboards,
-        [state.currentSoundboard]: {
-          ...state.currentSoundboards[state.currentSoundboard],
-          sounds: [
-            ...state.currentSoundboard,
-            {name: action.name, src: action.src}
-          ]
-        }
-      ]
-    }
-    case 'SET_VIEW': return {...state, view: action.view}
-    case 'SET_SOUNDBOARD': return {
-      ...state,
-      soundboard: state.currentSoundboards[action.index],
-      view: 'soundboard'
-    }
-    default: return state
-  }
+  soundboards: {0: {...soundboardTemplate, name: 'Default'}},
+  sounds: {0: {name: 'Beep', src: '/beep.mp3'}}
 }
